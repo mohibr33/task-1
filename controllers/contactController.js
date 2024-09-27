b90beebb31e1b6f7088fdebb78054cfa38d3adc3
mohibr33 <165779601+mohibr33@users.noreply.github.com>
2024-09-27 11:46:36 +0500

    Update contactController.js

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,25 +1,16 @@
-// controllers/contactController.js
-const contactModel = require("../models/contactModel");
+const contactModel= require("../models/contactModel");
 
 const addContact = (req, res) => {
-  const { name, email, message } = req.body;
-
-  if (!name || !email || !message) {
-    return res.status(400).send("Please provide your name, email, and message");
-  }
-
+  const contactData = req.body;
 
   contactModel.addContact(contactData, (err, result) => {
     if (err) {
-      return res.status(500).send("Internal Server Error");
+      return res.status(500).send(err.message);
     }
 
     res.status(200).json({
       message: "User has been added successfully!",
-      id: result.insertId,
-      name: name,
-      email: email,
-      message: message,
+      id: result.insertId, 
     });
   });
 };
