8ccbc4857bfe911b59d0720c54980df2c4b1f754
mohibr33 <165779601+mohibr33@users.noreply.github.com>
2024-09-27 11:46:01 +0500

    Update contactModel.js

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,15 +1,31 @@
-// models/contactModel.js
-const db=require("../db");
+const connection = require("../db");
 
-const addContact = (contactData, callback) => {
-  const { name, email, message } = contactData;
-  const query = `INSERT INTO form (name, email, message) VALUES (?, ?, ?)`;
-  
-  db.query(query, [name, email, message], (err, result) => {
-    if (err) {
-      return callback(err, null);
+class Contact {
+  constructor(name, email, message) {
+    this.name = name;
+    this.email = email;
+    this.message = message;
+  }
+  check() {
+    if (!this.name || !this.email || !this.message) {
+      throw new Error("All fields are required.");
     }
-    callback(null, result);
+  }
+}
+const addContact = (contactData, callback) => {
+  const contact = new Contact(contactData.name, contactData.email, contactData.message); //instance
+  
+  try {
+    contact.check();  
+  } catch (error) {
+    return callback(error, null); 
+  }
+  const query = `INSERT INTO form (name, email, message) VALUES (?, ?, ?)`;
+    connection.query(query, [contact.name, contact.email, contact.message], (err, result) => {
+    if (err) {
+      return callback(err, null); 
+    }
+    callback(null, result); 
   });
 };
 
