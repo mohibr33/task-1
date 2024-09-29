429862d22fda887161b5678e4208fe8ce529ad50
mohibr33 <165779601+mohibr33@users.noreply.github.com>
2024-09-29 14:07:52 +0500

    Updated database connection and added .env file for credentials

--- db.js
+++ db.js
@@ -1,16 +1,17 @@
-// db.js
 const mysql = require("mysql2");
+require("dotenv").config({ path: './log.env' }); // Specify the path to your custom env file
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "contact",
+  host: process.env.MYSQL_ADDON_HOST || "localhost",  // Use environment variable or default to localhost
+  user: process.env.MYSQL_ADDON_USER || "root",        // Use environment variable or default to root
+  password: process.env.MYSQL_ADDON_PASSWORD || "",     // Use environment variable or empty for no password
+  database: process.env.MYSQL_ADDON_DB || "contact",    // Use environment variable or default to contact
+  port: process.env.MYSQL_ADDON_PORT || 3306,           // Use environment variable or default to 3306
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error("Error connecting to the database:");
+    console.error("Error connecting to the database:", err);
     return;
   }
   console.log("Connected to the MySQL database");
