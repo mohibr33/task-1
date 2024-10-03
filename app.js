69d372541ead72a7576f119125b2e63fc0cb3003
mohibr33 <165779601+mohibr33@users.noreply.github.com>
2024-10-03 23:47:58 +0500

    Updated

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// app.js
 const express = require("express");
 const contactRoutes = require("./routes/contactRoutes");
 const app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
 // Define routes
